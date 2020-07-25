9c09d28fc00c7c945b2fd5b0418eff721733788f
Erce Amet <erce.amet@gmail.com>
2020-07-25 17:42:12 +0100

    up

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,7 +3,7 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-  apiKey: "",
+  apiKey: "AIzaSyBJJtAxZysgynguNKCQTmkKzhVFu6w2578",
   authDomain: "ecommerce-project-25c3e.firebaseapp.com",
   databaseURL: "https://ecommerce-project-25c3e.firebaseio.com",
   projectId: "ecommerce-project-25c3e",
